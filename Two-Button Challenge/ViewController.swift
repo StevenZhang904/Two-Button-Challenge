b53c83070382e79f983f2545d5b6061029a04fc5
STEVEN ZHANG <zhangbsr@bc.edu>
2020-01-19 16:36:44 -0500

    2.8 Video

--- Two-Button Challenge/ViewController.swift
+++ Two-Button Challenge/ViewController.swift
@@ -13,13 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber=0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        let awesomeMessage="You are awesome!"
+       /* let awesomeMessage="You are awesome!"
         if messageLabel.text==awesomeMessage{
             messageLabel.text="You are great!"
             imageView.image=UIImage(named:"image1")
@@ -28,7 +30,13 @@ class ViewController: UIViewController {
             messageLabel.text=awesomeMessage
             imageView.image=UIImage(named:"image0")
         }
-        
+        */
+        let imageName = "image"+String(imageNumber)
+        imageView.image=UIImage(named:imageName)
+        imageNumber=imageNumber+1
+        if imageNumber==10{
+            imageNumber=0
+        }
     }
 
 }
